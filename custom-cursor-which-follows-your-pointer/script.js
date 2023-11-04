fe93b9ee69942eb03df8c7411a8b68a31fb38692
Sunzie <sunzie.baba@gmail.com>
2023-11-05 00:19:28 +0530

    with better transition for hidden div

--- custom-cursor-which-follows-your-pointer/script.js
+++ custom-cursor-which-follows-your-pointer/script.js
@@ -16,14 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
   let buz = document.getElementById("baz");
   
     buz.addEventListener("mouseover", function() {
-      gsap.to(".cursor", 0.2,{scale:2, opacity:0.5, backgroundColor: '#F191AC'})
+      gsap.to(".cursor", 0.2,{scale:0.8, backgroundColor: '#F191AC'})
       gsap.to("#getBig", {visibility: 'hidden'})
       // gsap.to("#baz", 0.2, {color: '#F4BBC9'})
       // gsap.to('body', { duration: 0.5, backgroundColor: '#F191AC'})
     })
 
     buz.addEventListener("mouseleave", function() {
-      gsap.to(".cursor", 0.2,{scale:1, opacity:1, backgroundColor: '#007794'})
+      gsap.to(".cursor", 0.2,{scale:2, backgroundColor: '#007794'})
       gsap.to("#getBig", {visibility: 'hidden'})
       gsap.to("#baz", 0.2, {color: '#007794'})
       gsap.to('body', { duration: 0.5, backgroundColor: '#72DDF7'})
@@ -35,11 +35,16 @@ document.addEventListener("DOMContentLoaded", function() {
     buz.addEventListener("click", function() {
 
             var tl = gsap.timeline();
-            tl.to("#getBig", 0.1, {opacity: 1, visibility: 'visible', color: '#F191AC'})
-            tl.to("#getBig", {duration: 2, scale: 50,  ease: "power3"})
-            tl.to("#pink", {visibility: 'visible'}, "-=1.5")
+            tl.to("#getBig", 0.05, {opacity: 1, visibility: 'visible', color: '#F191AC'})
+            tl.to(".main-c", {visibility: 'hidden'})
+            // tl.to("#getBig", {duration: 0.4, scale: 0.4,  ease: "power3"}, "-=0.5")
+            tl.to("#getBig", {duration: 1.5, scale: 50,  ease: "power3"}, "-=0.5")
+            tl.to("#pink", {visibility: 'visible'}, "-=1")
             tl.to("#container", {visibility: 'collapse'})
             tl.to("#getBig", {visibility: 'hidden'})
+
+            var tl2 = gsap.timeline();
+            t12.from("#pink", {duration: 1, y: -100,  ease: "power3"})
     })
 
 
