fd415b37a95b6114b8ae110533fe415d55909a0e
Sunzie <sunzie.baba@gmail.com>
2023-11-05 00:06:00 +0530

    with better transition for hidden div

--- custom-cursor-which-follows-your-pointer/script.js
+++ custom-cursor-which-follows-your-pointer/script.js
@@ -17,14 +17,14 @@ document.addEventListener("DOMContentLoaded", function() {
   
     buz.addEventListener("mouseover", function() {
       gsap.to(".cursor", 0.2,{scale:2, opacity:0.5, backgroundColor: '#F191AC'})
-      gsap.to("#getBig", {opacity: 0})
+      gsap.to("#getBig", {visibility: 'hidden'})
       // gsap.to("#baz", 0.2, {color: '#F4BBC9'})
       // gsap.to('body', { duration: 0.5, backgroundColor: '#F191AC'})
     })
 
     buz.addEventListener("mouseleave", function() {
       gsap.to(".cursor", 0.2,{scale:1, opacity:1, backgroundColor: '#007794'})
-      gsap.to("#getBig", {opacity: 0})
+      gsap.to("#getBig", {visibility: 'hidden'})
       gsap.to("#baz", 0.2, {color: '#007794'})
       gsap.to('body', { duration: 0.5, backgroundColor: '#72DDF7'})
     })
@@ -35,17 +35,11 @@ document.addEventListener("DOMContentLoaded", function() {
     buz.addEventListener("click", function() {
 
             var tl = gsap.timeline();
-            // tl.set(".cursor", {opacity: 0})
-            tl.to("#getBig", 0.1, {opacity: 1, color: '#F191AC'})
-            tl.to("#getBig", 0.8, {width: '100%', height: '100%',  ease: "expoScale(0.5,7,none)"})
-
-            // gsap.to("#getBig", 1, {width: '100%', height: '100%',  ease: "Linear.easeNone", delay: 1 })
-            // gsap.to(".cursor", {opacity: 0})
-            // gsap.to("#getBig", 1, {opacity: 1})
-            // gsap.to("#getBig", 1, {color: '#F191AC'})
-            // gsap.to("#getBig", 1, {width: '100%', height: '100%',  ease: "Linear.easeNone", delay: 1 })
-            // gsap.to("#container", 2, {display: 'none',  ease: "power4.inOut", delay: 1 })
-            // gsap.to("#pink", 2, {display: 'flex',  ease: "power4.inOut", delay: 1 })
+            tl.to("#getBig", 0.1, {opacity: 1, visibility: 'visible', color: '#F191AC'})
+            tl.to("#getBig", {duration: 2, scale: 50,  ease: "power3"})
+            tl.to("#pink", {visibility: 'visible'}, "-=1.5")
+            tl.to("#container", {visibility: 'collapse'})
+            tl.to("#getBig", {visibility: 'hidden'})
     })
 
 
