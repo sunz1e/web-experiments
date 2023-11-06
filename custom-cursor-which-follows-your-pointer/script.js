ad50eed5a1304b6f5933905376ffab411157aed0
sunnysrivastava <mr.sunnysrivastava@gmail.com>
2023-11-06 11:18:45 +0530

    add on-click-page-transition-2-approach

--- custom-cursor-which-follows-your-pointer/script.js
+++ custom-cursor-which-follows-your-pointer/script.js
@@ -1,64 +1,18 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
 
-  gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+    var text = document.getElementById("indextext")
+    if(text != null){
+        text.addEventListener("click",function(event) {
+            event.preventDefault();
+            gsap.set("#indexOverlay", { visibility: "visible", width: "10px", height: "10px" });
+            gsap.to("#indexOverlay", {duration: 1, width: "100vw", height: "100vh", ease: "power3",  onComplete: () => {
+                // Animation is complete, navigate to the other page
+                 window.location.href = text.children[1].getAttribute('href')
+                console.log("done")
 
-  let xTo = gsap.quickTo(".cursor", "x", {duration: 0.3, ease: "elastic.out(1,1)"}),
-      yTo = gsap.quickTo(".cursor", "y", {duration: 0.4, ease: "elastic.out(1,1)"});
+            }}, )
+        })      
+    }
   
-  window.addEventListener("mousemove", e => {
-    xTo(e.clientX);
-    yTo(e.clientY);
-  });
-
-
-  let buz = document.getElementById("baz");
-  
-    buz.addEventListener("mouseover", function() {
-      gsap.to(".cursor", 0.2,{scale:0.5, backgroundColor: '#d87093'})
-      gsap.to("#getBig", {visibility: 'hidden'})
-      // gsap.to("#baz", 0.2, {color: '#F4BBC9'})
-      // gsap.to('body', { duration: 0.5, backgroundColor: '#F191AC'})
-    })
-
-    buz.addEventListener("mouseleave", function() {
-      gsap.to(".cursor", 0.2,{scale:1, backgroundColor: '#007794'})
-      gsap.to("#getBig", {visibility: 'hidden'})
-      gsap.to("#baz", 0.2, {color: '#007794'})
-      gsap.to('body', { duration: 0.5, backgroundColor: '#72DDF7'})
-    })
-
-   
-    var animating = false;
-
-    buz.addEventListener("click", function() {
-
-
-        if(animating == false){
-          console.log("came inside the animation loop")
-          animating = true;
-          var tl = gsap.timeline({onComplete: function() {
-            animating = false;
-          }});
-          tl.to("#getBig", 0.05, {opacity: 1, visibility: 'visible', color: '#d87093'})
-          tl.to(".main-c", {visibility: 'hidden'})
-          // tl.to("#getBig", {duration: 0.4, scale: 0.4,  ease: "power3"}, "-=0.5")
-          tl.to("#getBig", {duration: 1.5, scale: 50,  ease: "power3"}, "-=0.5")
-          tl.to("#getBig", {visibility: 'hidden'})            
-          tl.to("#pink", {visibility: 'visible'}, "-=1")
-          tl.to("#container", {visibility: 'collapse'})
-   
-
-          var tl2 = gsap.timeline();
-          tl2.from("#pinkinner", {duration: 2, y: '120vh',  ease: "power3"})
-          tl2.to("#pinkinner", {scale: 2 ,ease: "power3"})
-         
-          console.log("going outside of the animation loop")
-        }
-        console.log("completed the animation loop")
-
-    })
-
-
-});
+})
