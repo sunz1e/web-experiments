3873b71800442023d0709289d03fa82bf1237c7b
sunnysrivastava <mr.sunnysrivastava@gmail.com>
2023-11-06 13:24:59 +0530

    add on-click-page-transition-2-approach works for all positions of link div

--- custom-cursor-which-follows-your-pointer/script.js
+++ custom-cursor-which-follows-your-pointer/script.js
@@ -1,16 +1,68 @@
 
 document.addEventListener("DOMContentLoaded", function() {
+    gsap.set("#indexOverlay", { visibility: "visible", width: "10px", height: "10px" });
+
+    var overlay = document.getElementById("indexOverlay")
+
+    const divRect = overlay.getBoundingClientRect();
+
+    // Calculate the spaces
+    const spaceLeft = divRect.left;
+    console.log(spaceLeft)
+
+    const spaceRight = window.innerWidth - (divRect.left + divRect.width);
+    console.log(spaceRight)
+
+    const spaceTop = divRect.top;
+    console.log(spaceTop)
+
+    const spaceBottom = window.innerHeight - (divRect.top + divRect.height);
+    console.log(spaceBottom)
+
+    const horizontal = Math.max(spaceLeft, spaceRight);
+    const vertical = Math.max(spaceBottom, spaceTop);
+
+    console.log(horizontal)
+    console.log(vertical)
+    console.log(spaceLeft + spaceRight + divRect.width)
+    console.log(spaceBottom+spaceTop +divRect.height)
 
     var text = document.getElementById("indextext")
     if(text != null){
         text.addEventListener("click",function(event) {
             event.preventDefault();
-            gsap.set("#indexOverlay", { visibility: "visible", width: "10px", height: "10px" });
-            gsap.to("#indexOverlay", {duration: 1, width: "100vw", height: "100vh", ease: "power3",  onComplete: () => {
+           
+            var overlay = document.getElementById("indexOverlay")
+
+            const divRect = overlay.getBoundingClientRect();
+
+            // Calculate the spaces
+            const spaceLeft = divRect.left;
+            console.log(spaceLeft)
+
+            const spaceRight = window.innerWidth - (divRect.left + divRect.width);
+            console.log(spaceRight)
+
+            const spaceTop = divRect.top;
+            console.log(spaceTop)
+
+            const spaceBottom = window.innerHeight - (divRect.top + divRect.height);
+            console.log(spaceBottom)
+
+            const horizontal = Math.max(spaceLeft, spaceRight);
+            const vertical = Math.max(spaceBottom, spaceTop);
+
+            console.log(horizontal)
+            console.log(vertical)
+            console.log(spaceLeft + spaceRight + divRect.width)
+            console.log(spaceBottom+spaceTop +divRect.height)
+            
+ 
+            gsap.to("#indexOverlay", {duration: 1, width: (2 * horizontal + divRect.width) +"px", height: (2 * vertical + divRect.height) +"px", ease: "power3",  onComplete: () => {
                 // Animation is complete, navigate to the other page
                  window.location.href = text.children[1].getAttribute('href')
                 console.log("done")
-
+                
             }}, )
         })      
     }
