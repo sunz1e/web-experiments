393839f22471a7598165c0a8b2eb923088e57395
sunnysrivastava <mr.sunnysrivastava@gmail.com>
2023-11-11 20:43:07 +0530

    loader

---
+++ loading/script.js
@@ -0,0 +1,34 @@
+function getScreenFactor() {
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  
+    if (viewportWidth < 600) {
+      // Mobile screen
+      return 2;
+    } else if (viewportWidth < 1200) {
+      // Normal screen
+      return 3;
+    } else {
+      // Widescreen
+      return 4;
+    }
+  }
+
+
+var tl = gsap.timeline({repeat: -1, delay: 0.1})
+
+tl.from(".loader", { x: "100vw", duration: getScreenFactor() / 2,
+  
+    stagger: function (index, target, list) {
+        // your custom logic here. Return the delay from the start (not between each)
+        return 0.3 - (0.05 * index);
+    },
+  ease: "power3"},);
+
+
+
+  tl.to(".loader", { x: "-100vw", duration: getScreenFactor() / 2, stagger: function (index, target, list) {
+    // your custom logic here. Return the delay from the start (not between each)
+    return 0.3 - (0.05 * index) ;
+  },
+  ease: "power3"});
+
