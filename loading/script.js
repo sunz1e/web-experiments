004b3ae8f47dc12f0a2ec1b0bb264902f0fb9dbf
sunz1e <hi.sunzie@gmail.com>
2023-11-18 09:06:19 +0530

    add horizontal scroll on vertical wheel movement

--- loading/script.js
+++ loading/script.js
@@ -4,19 +4,28 @@ function getScreenFactor() {
     if (viewportWidth < 600) {
       // Mobile screen
       return 2;
-    } else if (viewportWidth < 1200) {
+    } else if (viewportWidth < 2000) {
       // Normal screen
-      return 3;
+      return 4;
     } else {
       // Widescreen
-      return 4;
+      return 5;
     }
   }
 
 
+  var overlay = document.getElementsByClassName("loader")
+  const divRect = overlay[3].getBoundingClientRect();
+  const divRect1 = overlay[0].getBoundingClientRect();
+
+console.log(overlay)
+console.log(divRect.left)
+console.log(divRect1.right)
+var variance = divRect1.right - divRect.left
+
 var tl = gsap.timeline({repeat: -1, delay: 0.1})
 
-tl.from(".loader", { x: "51vw", duration: getScreenFactor() / 2,
+tl.from(".loader", { x: (53.5) + "vw", duration: getScreenFactor() / 2,
   
     stagger: function (index, target, list) {
         // your custom logic here. Return the delay from the start (not between each)
@@ -26,10 +35,7 @@ tl.from(".loader", { x: "51vw", duration: getScreenFactor() / 2,
 
 
 
-  tl.to(".loader", { x: "-52vw", duration: getScreenFactor() / 2, stagger: function (index, target, list) {
-    // your custom logic here. Return the delay from the start (not between each)
-    return 0.3 - (0.05 * index) ;
-  },
+  tl.to(".loader", { x: -1 * (50 + variance/9) + "vw", duration: getScreenFactor() / 2, stagger: 0.1,
   ease: "power3",
   onComplete: function() {
       console.log("Animation complete");
